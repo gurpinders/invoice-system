602e224379a863fd80c4e7d9566c2d3cf39ecdf2
Gurpinderjeet Sandhu <psandhu0124@gmail.com>
2025-10-20 00:07:52 -0400

    Built createInvoice and addEntry endpoints - tested successfully with Postman

--- Server/controllers/invoiceController.js
+++ Server/controllers/invoiceController.js
@@ -1,5 +1,32 @@
 import Invoice from "../models/Invoice.js";
 
+async function createInvoice(req, res) {
+  try {
+    const { invoiceNumber } = req.body;
+    const newInvoice = await Invoice.create({ invoiceNumber });
+    res.status(201).json(newInvoice);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function addEntry(req, res) {
+  try {
+    const { id } = req.params;
+    const { date, ticket, haulFrom, haulTo, weight, ratePerTonne } = req.body;
+    const invoice = await Invoice.findById(id);
+    if(!invoice){
+      return res.status(404).json({ message: "Invoice Not Found! "});
+    }
+    invoice.entries.push({ date, ticket, haulFrom, haulTo, weight, ratePerTonne });
+    const addedEntry = await invoice.save();
+    res.status(200).json(addedEntry);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 async function getAllInvoices(req, res) {
   try {
     const invoices = await Invoice.find();
@@ -33,24 +60,24 @@ async function getInvoicesById(req, res) {
     if (!invoice) {
       return res.status(404).json({ message: "Invoice Not Found " });
     }
-    const subtotal = invoice.entries.reduce((sum,entry) => {
-        sum += entry.weight * entry.ratePerTonne;
-        return sum;
+    const subtotal = invoice.entries.reduce((sum, entry) => {
+      sum += entry.weight * entry.ratePerTonne;
+      return sum;
     }, 0);
     const feeCut = subtotal * 0.07;
     const total = subtotal - feeCut;
     res.status(200).json({
-        _id: invoice._id,
-        invoiceNumber: invoice.invoiceNumber,
-        createdAt: invoice.createdAt,
-        entries: invoice.entries,
-        subtotal: subtotal,
-        feeCut: feeCut,
-        total: total,
-    })
+      _id: invoice._id,
+      invoiceNumber: invoice.invoiceNumber,
+      createdAt: invoice.createdAt,
+      entries: invoice.entries,
+      subtotal: subtotal,
+      feeCut: feeCut,
+      total: total,
+    });
   } catch (error) {
-    res.status(500).json({message: error.message});
+    res.status(500).json({ message: error.message });
   }
 }
 
-export { getAllInvoices, getInvoicesById };
+export { getAllInvoices, getInvoicesById, createInvoice, addEntry };
--- Server/routes/invoiceRoutes.js
+++ Server/routes/invoiceRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllInvoices, getInvoicesById } from "../controllers/invoiceController.js";
+import { createInvoice, getAllInvoices, getInvoicesById, addEntry } from "../controllers/invoiceController.js";
 
 const router = Router();
 
 router.get("/", getAllInvoices);
 router.get("/:id", getInvoicesById);
+router.post("/", createInvoice);
+router.post("/:id/entries", addEntry);
 
 export default router;
