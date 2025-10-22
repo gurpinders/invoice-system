94bb42ca2422331faec85789a34a032714ea9fe1
Gurpinderjeet Sandhu <psandhu0124@gmail.com>
2025-10-22 14:14:47 -0400

    Type fix for date

--- Client/src/pages/InvoiceForm.jsx
+++ Client/src/pages/InvoiceForm.jsx
@@ -332,7 +332,7 @@ function InvoiceForm(){
             <tbody>
                 {entries.map((entry) => (
                 <tr key={entry._id}>
-                    <td className="px-4 py-3 border-t border-gray-200 text-gray-700">{entry.date.split('T')[0]}}</td>
+                    <td className="px-4 py-3 border-t border-gray-200 text-gray-700">{entry.date.split('T')[0]}</td>
                     <td className="px-4 py-3 border-t border-gray-200 text-gray-700">{entry.ticket}</td>
                     <td className="px-4 py-3 border-t border-gray-200 text-gray-700">{entry.haulFrom}</td>
                     <td className="px-4 py-3 border-t border-gray-200 text-gray-700">{entry.haulTo}</td>
